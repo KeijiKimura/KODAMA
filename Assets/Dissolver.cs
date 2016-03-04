05b4f937eab7f4d7a2e2dbf3c72acd97fdbbb18c
KeijiKimura <kimuk170@newschool.edu>
2016-03-03 21:48:32 -0500

    GAME

---
+++ Assets/Dissolver.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Dissolver : MonoBehaviour {
+    public Renderer rend;
+
+	// Use this for initialization
+	void Start () {
+       /* rend= GetComponent<Renderer>();
+        rend.material.shader = Shader.Find("DissolveShader");
+        rend.material.SetFloat("_SliceAmount", 1.0f);*/
+    }
+	
+	// Update is called once per frame
+	void Update () {
+       
+
+    }
+}
